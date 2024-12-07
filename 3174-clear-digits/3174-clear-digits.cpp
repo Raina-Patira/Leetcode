2dc07a722f396bd2306986ece4912bb7bfdb1dd9
Raina Patira <114555023+Raina-Patira@users.noreply.github.com>
2024-12-07 08:54:04 +0530

    Time: 3 ms (15.67%), Space: 9 MB (29.50%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a' && s[i]<='z') st.push(s[i]);
+            else st.pop();
+        }
+        string res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
